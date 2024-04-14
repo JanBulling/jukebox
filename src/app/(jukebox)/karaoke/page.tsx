065bb9ad24bd18802e1dfd08b90a8bdc60e9a86e
Jan Bulling <jan.greenengage@gmail.com>
2024-04-14 16:55:31 +0200

    changed caching for search

--- src/app/(jukebox)/karaoke/page.tsx
+++ src/app/(jukebox)/karaoke/page.tsx
@@ -3,21 +3,21 @@ import { db } from "@/lib/db/db";
 import { spotifyIdToYoutubeIdTable } from "@/lib/db/schema";
 import { sql } from "drizzle-orm";
 
-export const dynamic = "auto";
-export const revalidate = 12 * 60 * 60;
+// export const dynamic = "auto";
+// export const revalidate = 12 * 60 * 60;
 
-export default async function Jukebox() {
-  const [count] = await db
-    .select({
-      count: sql<number>`count(*)`,
-    })
-    .from(spotifyIdToYoutubeIdTable)
-    .where(sql`${spotifyIdToYoutubeIdTable.createdAt}::date = CURRENT_DATE`);
+export default function Jukebox() {
+  // const [count] = await db
+  //   .select({
+  //     count: sql<number>`count(*)`,
+  //   })
+  //   .from(spotifyIdToYoutubeIdTable)
+  //   .where(sql`${spotifyIdToYoutubeIdTable.createdAt}::date = CURRENT_DATE`);
 
   return (
     <>
       <h1 className='text-5xl font-semibold'>Karaoke</h1>
-      <p className='font-bold text-sm p-4'>Search: {count.count} / 99 today</p>
+      {/* <p className='font-bold text-sm p-4'>Search: {count.count} / 99 today</p> */}
       <Searchbar className='mt-8' forType={"karaoke"} />
     </>
   );
